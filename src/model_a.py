0eccf71ac8eb3ebeef6c068f28013b93a71096aa
James Cheng <qianfan.cheng.cn@gmail.com>
2024-03-17 00:30:37 -0400

    update data
    1. add test and val data
    2. add annotations
    3. update folder structure
    4. complete base model

--- src/model_a.py
+++ src/model_a.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from typing import Any, List
 
+import pandas as pd
 from ultralytics import YOLO
+from ultralytics.utils.benchmarks import benchmark
 
 from src.yolo import YoloModelInterface
 
@@ -11,6 +13,7 @@ class ModelA(YoloModelInterface):
     """
     implement the abstract methods
     """
+
     def __init__(self, model: str, data_cfg: str, device: Any):
         """
         initialize the model with pretrained model name and data configure file
@@ -21,11 +24,12 @@ class ModelA(YoloModelInterface):
         self.data_cfg = data_cfg
         self.device = device
 
-    def train(self, project_dir: str, conf: dict = None) -> tuple:
+    def train(self, project_dir: str, conf: dict = None) -> tuple[YOLO, dict | None]:
         """
         train the model
         :param project_dir:
-        :param conf: dictionary, may include: optimizer, momentum, freeze, batch, patience, epochs, tensorboard
+        :param conf: dictionary, may include: optimizer, momentum,
+                    freeze, batch, patience, epochs, tensorboard
         :return:
         """
         # data_cfg = 'yolo-data-conf.yaml'
@@ -33,8 +37,9 @@ class ModelA(YoloModelInterface):
         train_args = {
             # Options include SGD, Adam, AdamW, NAdam, RAdam, RMSProp etc., or auto
             "optimizer": conf.get('optimizer', 'Adam'),
+            "lr0": conf.get('learning_rate', 1e-4),
             "momentum": conf.get('momentum', 0.938),
-            "freeze": conf.get('freeze', 10),  # the first N layers
+            "freeze": conf.get('freeze', 16),  # the first N layers
             "batch": conf.get('batch', -1),
             "patience": conf.get('patience', 5),
             "epochs": conf.get('epochs', 3),
@@ -52,24 +57,32 @@ class ModelA(YoloModelInterface):
                              save=True,
                              amp=True,  # Enables Automatic Mixed Precision (AMP) training
                              exist_ok=True,
-                             pretrained=True, # Determines whether to start training from a pretrained model.
+                             pretrained=True,  # Determines whether to start training from a pretrained model.
                              verbose=True,
                              **train_args)
 
         return model, result
 
-    def val(self, model: YOLO, batch_size=-1) -> dict:
+    def val(self, model: YOLO, **kwargs) -> dict | None:
         """
-        evaluate model
-        :param model: YOLO, trained model
+        evaluate the model
+        :param model: YOLO model
+        :param kwargs: split | iou | conf | batch
         :return:
         """
+        excluded_dict = {key: value for key, value in kwargs.items() if key not in ('data', 'device')}
+        val_args = {
+            # Determines the dataset split to use for validation (val, test, or train)
+            "split": excluded_dict.get('split', 'val'),
+            "iou": excluded_dict.get('iou', 0.6),
+            "conf": excluded_dict.get('conf', 0.001),  # the confidence
+            "batch": excluded_dict.get('batch', 16),
+        }
+
         # Validate the model
         metrics = model.val(data=self.data_cfg,
-                            batch=batch_size,
-                            conf=0.25,
-                            iou=0.6,
-                            device=self.device)
+                            device=self.device,
+                            **val_args)
         # metrics.box.map  # map50-95
         # metrics.box.map50  # map50
         # metrics.box.map75  # map75
@@ -77,18 +90,30 @@ class ModelA(YoloModelInterface):
 
         return metrics
 
-    def predict(self, model: YOLO, conf: float = 0.5) -> List:
+    def predict(self, model: YOLO, img: str, conf: float = 0.5) -> List:
         # Run inference on 'bus.jpg' with arguments
-        result = model.predict('bus.jpg', save=True, conf=conf)
+        result = model.predict(source=img,
+                               save=True,
+                               conf=conf)
 
         return result
 
-    def export(self):
-        pass
-
+    def export(self, model: YOLO, fmt: str = '-') -> Any:
+        return model.export(format=fmt)
 
     def track(self):
         pass
 
-    def benchmark(self):
-        pass
+    def benchmark(self, **kwargs) -> pd.DataFrame:
+        excluded_dict = {key: value for key, value in kwargs.items()
+                         if key not in ('model', 'data', 'device')}
+        bm_args = {
+            "model": excluded_dict.get('split', 'val'),
+            "verbose": excluded_dict.get('verbose', True),
+        }
+        bm = benchmark(model=self.model,
+                       data=self.data_cfg,
+                       device=self.device,
+                       **bm_args)
+
+        return bm
